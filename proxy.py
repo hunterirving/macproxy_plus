22734b1574c35d1f019216cce0d1a3581fa73e08
Daniel Markstedt <markstedt@gmail.com>
2021-11-06 12:12:33 -0700

    Retain the original content-type header after proxying

--- proxy.py
+++ proxy.py
@@ -1,6 +1,6 @@
 from macify import macify
 import requests
-from flask import request, Flask
+from flask import Flask, request, session, g
 
 app = Flask(__name__)
 session = requests.Session()
@@ -10,6 +10,7 @@ session = requests.Session()
 def get(path):
     url = request.url.replace('https://', 'http://', 1)
     resp = session.get(url, params=request.args)
+    g.contenttype = resp.headers['Content-Type']
     if resp.headers['Content-Type'].startswith("text/html"):
         return macify(resp.content), resp.status_code
     return resp.content, resp.status_code
@@ -19,9 +20,17 @@ def get(path):
 def post(path):
     url = request.url.replace('https://', 'http://', 1)
     resp = session.post(url, data=request.form, allow_redirects=True)
+    g.contenttype = resp.headers['Content-Type']
     if resp.headers['Content-Type'].startswith("text/html"):
         return macify(resp.content), resp.status_code
     return resp.content, resp.status_code
+
+@app.after_request
+def apply_caching(resp):
+    # Workaround for retaining the Content-Type header for f.e. downloading binary files.
+    # There may be a more elegant way to do this.
+    resp.headers['Content-Type'] = g.contenttype
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
