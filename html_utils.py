b314f946597d1ecd8733c2744e5f42f8fcc12e7d
Daniel Markstedt <markstedt@gmail.com>
2021-11-06 22:03:02 -0700

    Remove unused code

--- html_utils.py
+++ html_utils.py
@@ -14,11 +14,3 @@ def transcode_html(html):
         for attr in ['style', 'onclick']:
             del tag[attr]
     return str(soup)
-
-if __name__ == '__main__':
-    import requests
-    html = requests.get('http://stackoverflow.com/questions/5598524/can-i-remove-script-tags-with-beautifulsoup').content
-    html = macify(html)
-    with open('macified.html', 'w') as fd:
-        fd.write(html)
-
